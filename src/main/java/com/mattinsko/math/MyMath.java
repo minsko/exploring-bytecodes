152dc915230e4107b995de63267f1535374ff55f
Matt Insko <minsko@preemptive.com>
2014-01-10 10:08:00 -0500

    Adding Math

---
+++ src/main/java/com/mattinsko/math/MyMath.java
@@ -0,0 +1,24 @@
+package com.mattinsko.math;
+
+public class MyMath {
+
+public int add(int i, int j) {
+	return i+j;
+}
+
+public long div(long i, long j) {
+	return i/j;
+}
+
+public int mod(int i, int j) {
+	return i%j;
+}
+
+public double mult(double d, double e) {
+	return d * e;
+}
+
+public float times2(float f) {
+	return f*2;
+}
+}
