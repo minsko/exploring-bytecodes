49df59b3414715cb502acda31c1a0c8a9dd49361
Matt Insko <minsko@preemptive.com>
2014-01-10 13:33:23 -0500

    More math

---
+++ src/main/java/com/mattinsko/math/MyMath2.java
@@ -0,0 +1,24 @@
+package com.mattinsko.math;
+
+public class MyMath2 {
+
+public int w(int i, int j) {
+	return i+j;
+}
+
+public long w(long i, long j) {
+	return i/j;
+}
+
+public int vv(int i, int j) {
+	return i%j;
+}
+
+public double w(double d, double e) {
+	return d * e;
+}
+
+public float w(float f) {
+	return f*2;
+}
+}
