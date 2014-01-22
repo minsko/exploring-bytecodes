70d18287948128f721ace1d300f9428b2fb18519
Matt Insko <minsko@preemptive.com>
2014-01-22 10:26:39 -0500

    Updated Java8 Example

--- Java8/Java8.java
+++ Java8/Java8.java
@@ -1,6 +1,16 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Java8 {
+
+	public static void main (String args[]) {
+		Java8 j8 = new Java8();
+		List<String> list = new ArrayList<String>();
+		list.add("Apple");
+		list.add("Banana");
+		list.add("IBM");
+		j8.loop(list);
+	}
 
 	public void loop(List<String> vals) {
 		vals.forEach(s -> System.out.println(s));
